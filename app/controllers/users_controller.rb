5cd883ecd7ed11b4bf666d8bc21db3e0b268a018
Ariel Leyva <aleyva1509@gmail.com>
2024-04-27 00:28:13 -0600

    get for user done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,46 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-    protect_from_forgery with: :null
+    protect_from_forgery with: :null_session
     before_action :authenticate_request!, only: [:show, :index]
 
     def show
         user_id = params[:id]
         user_doc = FirestoreDB.doc("users/#{user_id}").get
-        # quitamos el password del usuario
-        user_doc.data.delete(:password)
 
-        if user_doc.exists?
-        user_data = user_doc.data
-        render json: { user: user_data }, status: :ok
+        if user_doc['active']
+            user_data = user_doc
+            user_obj = {
+                id: user_id,
+                nombre_completo: user_data['nombre_completo'],
+                correo: user_data['correo'],
+                celular: user_data['celular'],
+                campus: user_data['campus'],
+                tipo: user_data['tipo'],
+                identificador: user_data['identificador']
+            }
+            render json: user_obj
         else
         render json: { error: 'User not found' }, status: :not_found
         end
     end
 
     def index
-        users = FirestoreDB.col('users').get
-        # quitamos el password de los usuarios
-        users.each { |user_doc| user_doc.data.delete(:password) }
-        user_data = users.map { |user_doc| user_doc.data }
-        render json: { users: user_data }, status: :ok
+        users_ref = FirestoreDB.col('users')
+        users = users_ref.where('active', '==', true).get
+        # translate each user into a hash of useful fields
+        users_data = users.map do |user|
+            user_data = user 
+            user_obj = {
+                id: user.document_id,
+                nombre_completo: user_data['nombre_completo'],
+                correo: user_data['correo'],
+                celular: user_data['celular'],
+                campus: user_data['campus'],
+                tipo: user_data['tipo'],
+                identificador: user_data['identificador']
+            }
+            user_obj 
+        end
+        render json: users_data
     end
 end
