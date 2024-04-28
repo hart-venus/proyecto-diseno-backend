7bcafa2e9808c87d999eb150b1881d25efe3d61a
Ariel Leyva <aleyva1509@gmail.com>
2024-04-28 13:21:37 -0600

    add: custom firestore startup for hosting

--- config/initializers/firestore.rb
+++ config/initializers/firestore.rb
@@ -1,3 +1,15 @@
 require 'google/cloud/firestore'
 
-FirestoreDB = Google::Cloud::Firestore.new 
+if ENV['GOOGLE_APPLICATION_CREDENTIALS'].present?
+    # Use the GOOGLE_APPLICATION_CREDENTIALS environment variable
+    FirestoreDB = Google::Cloud::Firestore.new
+else
+    # Use the individual environment variables for credentials
+    FirestoreDB = Google::Cloud::Firestore.new(
+        project_id: ENV['GOOGLE_PROJECT_ID'],
+        credentials: {
+            private_key: ENV['GOOGLE_PRIVATE_KEY'].gsub('\\n', "\n"),
+            client_email: ENV['GOOGLE_CLIENT_EMAIL']
+        }
+    )
+end
