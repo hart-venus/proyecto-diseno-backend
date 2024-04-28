fa1aeac926aab7eff27bc916d2510aa0160f6b3e
Ariel Leyva <aleyva1509@gmail.com>
2024-04-28 01:11:52 -0600

    feat: add plan cruds

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,57 @@
+class PlansController < ApplicationController 
+    protect_from_forgery with: :null_session
+    before_action :authenticate_request!
+    before_action :authorize_admin_or_coordinator, only: [:create, :update]
+    before_action :set_plan, only: [:show, :update]
+
+    def index 
+        plans_ref = FirestoreDB.col('plans').where('active', '==', true)
+        plans = plans_ref.get.map do |plan|
+            { id: plan.document_id }.merge(plan.data)
+        end
+        render json: plans
+    end
+
+    def show
+        render json: { id: @plan.document_id }.merge(@plan.data)
+    end
+
+    def create
+        plan_params = params.require(:plan).permit(:nombre, :sede)
+        plan = Plan.new(plan_params)
+        if plan.valid?
+            plan_data = {
+                nombre: plan_params[:nombre],
+                sede: plan_params[:sede],
+                active: true
+            }
+            plan_ref = FirestoreDB.col('plans').add(plan_data)
+            render json: { id: plan_ref.document_id }, status: :created
+        else
+            render json: { errors: plan.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        target_plan_id = params[:id]
+        target_plan_ref = FirestoreDB.doc("plans/#{target_plan_id}")
+
+        plan_params = params.require(:plan).permit(:nombre, :sede, :active)
+        target_plan_ref.set(plan_params.to_h, merge: true)
+        render json: { id: target_plan_id }, status: :ok
+    end
+
+
+    private
+
+    def set_plan
+        @plan = FirestoreDB.doc("plans/#{params[:id]}").get
+        render json: { error: 'Plan not found' }, status: :not_found unless @plan.exists?
+    end
+
+    def authorize_admin_or_coordinator
+        unless ['AsistenteAdmin', 'ProfesorCoordinador'].include?(@current_user[:tipo])
+            render json: { error: 'Not authorized' }, status: :forbidden
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   patch '/users/:id', to: 'users#update'
 
+  resources :plans, except: [:new, :edit, :destroy]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
