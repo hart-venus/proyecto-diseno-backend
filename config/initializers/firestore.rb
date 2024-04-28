ac54962803f3985cffed1cb06fb927959735ad58
Ariel Leyva <aleyva1509@gmail.com>
2024-04-28 15:11:29 -0600

    hotfix: credentials google

--- config/initializers/firestore.rb
+++ config/initializers/firestore.rb
@@ -1,15 +1,4 @@
 require 'google/cloud/firestore'
 
-if ENV['GOOGLE_APPLICATION_CREDENTIALS'].present?
-    # Use the GOOGLE_APPLICATION_CREDENTIALS environment variable
-    FirestoreDB = Google::Cloud::Firestore.new
-else
-    # Use the individual environment variables for credentials
-    FirestoreDB = Google::Cloud::Firestore.new(
-        project_id: ENV['GOOGLE_PROJECT_ID'],
-        credentials: {
-            private_key: ENV['GOOGLE_PRIVATE_KEY'].gsub('\\n', "\n"),
-            client_email: ENV['GOOGLE_CLIENT_EMAIL']
-        }
-    )
-end
+FirestoreDB = Google::Cloud::Firestore.new
+
