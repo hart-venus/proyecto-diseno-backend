bf7a4ca1c5692d092830156f07e82d23c97b1672
jcardonar <jcardonar@estudiantec.cr>
2024-05-02 09:36:07 -0600

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,49 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  post '/signup', to: 'auth#signup'
-  post '/login', to: 'auth#login'
-  get '/protected', to: 'test#protected'
-  get '/users/:id', to: 'users#show'
-  get '/users', to: 'users#index'
-  patch '/users/:id', to: 'users#update'
+  namespace :api do
+    namespace :v1 do
+      # Autenticación
+      post '/login', to: 'authentication#login'
+      post '/signup', to: 'authentication#signup'
 
-  resources :plans, except: [:new, :edit, :destroy]
-  resources :activities, except: [:new, :edit, :destroy]
-  resources :messages, except: [:new, :edit, :destroy, :update]
+      # Usuarios
+      get '/users', to: 'users#index'
+      get '/users/:id', to: 'users#show'
+      patch '/users/:id', to: 'users#update'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+      # Profesores Guía
+      get '/profesores', to: 'professors#index'
+      get '/profesores/:id', to: 'professors#show'
+      post '/profesores', to: 'professors#create'
+      patch '/profesores/:id', to: 'professors#update'
+      delete '/profesores/:id', to: 'professors#destroy'
+      post '/profesores/:id/coordinador', to: 'professors#assign_coordinator'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+      # Estudiantes
+      post '/estudiantes/cargar', to: 'students#load_data'
+      get '/estudiantes', to: 'students#index'
+      get '/estudiantes/:id', to: 'students#show'
+      patch '/estudiantes/:id', to: 'students#update'
+
+      # Planes de Trabajo
+      get '/planes', to: 'plans#index'
+      get '/planes/:id', to: 'plans#show'
+      post '/planes', to: 'plans#create'
+      patch '/planes/:id', to: 'plans#update'
+
+      # Actividades
+      get '/actividades', to: 'activities#index'
+      get '/actividades/:id', to: 'activities#show'
+      post '/actividades', to: 'activities#create'
+      patch '/actividades/:id', to: 'activities#update'
+      delete '/actividades/:id', to: 'activities#destroy'
+
+      # Comentarios
+      post '/actividades/:activity_id/comentarios', to: 'comments#create'
+      post '/actividades/:activity_id/comentarios/:comment_id/respuestas', to: 'comments#create_reply'
+
+      # Reportes
+      get '/reportes/estudiantes', to: 'reports#students'
+      get '/reportes/estudiantes/:campus', to: 'reports#students_by_campus'
+    end
+  end
 end
