31cb2be14274edd8147178f33ab858fb384cdf77
jcardonar <jcardonar@estudiantec.cr>
2024-10-13 14:36:09 -0600

    .

--- config/initializers/firebase.rb
+++ config/initializers/firebase.rb
@@ -1,5 +1,29 @@
 require 'google/cloud/firestore'
 require 'google/cloud/storage'
 
-FirestoreDB = Google::Cloud::Firestore.new
-FirebaseStorage = Google::Cloud::Storage.new
+if Rails.env.production?
+  # Use the credentials file specified by the environment variable
+  credentials = JSON.parse(ENV['GOOGLE_CREDENTIALS']) if ENV['GOOGLE_CREDENTIALS']
+  
+  # Configure Firestore
+  FirestoreDB = Google::Cloud::Firestore.new(
+    credentials: credentials
+  )
+  
+  # Configure Cloud Storage
+  FirebaseStorage = Google::Cloud::Storage.new(
+    credentials: credentials
+  )
+else
+  # Development environment setup
+  # Make sure to replace 'your-project-id' with your actual project ID
+  FirestoreDB = Google::Cloud::Firestore.new(
+    project_id: "projecto-diseno-backend",
+    credentials: "credentials.json"
+  )
+  
+  FirebaseStorage = Google::Cloud::Storage.new(
+    project_id: "projecto-diseno-backend",
+    credentials: "credentials.json"
+  )
+end
